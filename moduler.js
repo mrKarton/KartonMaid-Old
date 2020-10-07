e84da371858c91b313c644bac6548f268d3e48c7
mrKarton <gornostaev.dmitry04@gmail.com>
2020-10-07 20:32:40 +0400

    Hotfix

--- moduler.js
+++ moduler.js
@@ -8,11 +8,12 @@ var Tickets = require('./modules/Tickets.js');
 
 var fullList = Array();
 
-fullList.push({module: Adm.about, commands: Adm.commands});
-fullList.push({module: help.about, commands: help.commands});
-fullList.push({module: voice.about, commands: voice.commands});
-fullList.push({module: fun.about, commands: fun.commands});
-fullList.push({module:Tickets.about, commands: Tickets.commands});
+fullList.push({module: Adm.about,     commands: Adm.commands});
+fullList.push({module: Tickets.about, commands: Tickets.commands});
+fullList.push({module: help.about,    commands: help.commands});
+fullList.push({module: voice.about,   commands: voice.commands});
+fullList.push({module: fun.about,     commands: fun.commands});
+
 
 
 
